6f3b9fa73db9d556644c963bc139630b3acc2c6c
Rakesh kumar <rakeshkumaryadvanshi@gmail.com>
2020-05-08 08:56:57 +0530

    fix

--- Serilog-Examples/Program.cs
+++ Serilog-Examples/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using Serilog;
 
 namespace Serilog_Examples
 {
@@ -6,7 +8,32 @@ namespace Serilog_Examples
     {
         static void Main(string[] args)
         {
+
+            Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+            
+
+            using (var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger())
+            {
+                log.Information("Hello, Serilog!");
+                log.Warning("Goodbye, Serilog.");
+                var userfullObject = new VerUseFullClass();
+               var name= userfullObject.Name;
+               var id = userfullObject.Id;
+                log.Warning("Goodbye, Serilog. {name}, {id}", id,  name);
+            }
+
+
+
+
             Console.WriteLine("Hello World!");
+        }
+
+        class VerUseFullClass
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = "import name";
         }
     }
 }
